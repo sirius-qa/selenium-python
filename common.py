5029a009f2df99843876a1e37496611b83bc30ce
nicoulmete <nicolas.ulmete@mandiant.com>
2021-08-27 10:55:01 -0300

    delete bad files

--- common.py
+++
@@ -1,6 +0,0 @@
-# Common Locators go here
-class CustomButtonLocators:
-    BUTTON = """
-        //button[starts-with(@class, 'MuiButtonBase-root')]
-        /span[@class='MuiButton-label'][contains(text(), '{}')]
-    """
